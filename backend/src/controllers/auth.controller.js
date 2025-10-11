9a38ef2a3649adb6fbaf03598e74792724116c26
krishh-9085 <rohillakrish2@gmail.com>
2025-10-11 15:25:18 +0530

    welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import { sendWelcomeEmail } from "../emails/emailHandles.js";
+import { ENV } from "../lib/env.js";
+
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body
@@ -44,8 +47,14 @@ export const signup = async (req, res) => {
                 email: newUser.email,
                 profilePic: newUser.profilePic,
             });
-            // todo:send a welcome email to user
 
+
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+
+            } catch (error) {
+                console.log("failed sending welcome email:", error);
+            }
         } else {
             res.status(400).json({ message: "Invalid user data" });
         }
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
-
+import { ENV } from "./env.js";
 export const generateToken = (userId, res) => {
-    const {JWT_SECRET} = process.env;
+    const {JWT_SECRET} = ENV;
     if(!JWT_SECRET){
         throw new Error("JWT_SECRET is not configured")
     }
@@ -14,7 +14,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //prevent XSS attacks:scriss site scripting
         sameSite: "strict", //CRSF attack
-        secure: process.env.NODE_ENV === "development" ? false : true,
+        secure: ENV.NODE_ENV === "development" ? false : true,
     });
 
     return token;
