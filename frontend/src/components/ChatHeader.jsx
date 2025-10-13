0da0938421535288a2022bacd0baacff577d70e6
krishh-9085 <rohillakrish2@gmail.com>
2025-10-14 00:07:09 +0530

    cross-icon

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -22,7 +22,7 @@ function ChatHeader() {
   return (
     <div
       className="flex justify-between items-center bg-slate-800/50 border-b
-   border-slate-700/50 max-h-[84px] px-6 flex-1"
+   border-slate-700/50 max-h-[84px] px-6 flex-1 w-full"
     >
       <div className="flex items-center space-x-3">
         <div className={`avatar ${isOnline ? "online" : "offline"}`}>
@@ -37,7 +37,7 @@ function ChatHeader() {
         </div>
       </div>
 
-      <button onClick={() => setSelectedUser(null)}>
+      <button onClick={() => setSelectedUser(null)} className="block">
         <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
       </button>
     </div>
