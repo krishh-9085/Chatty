471cda6e244cf864d9c007f85d6ab2cbf8889895
Krishna Rohilla <rohillakrish2@gmail.com>
2026-01-28 23:24:00 +0530

    fix render error

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,15 +21,15 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-//make ready for deployment
-if (ENV.NODE_ENV === "production") {
-    app.use(express.static(Path.join(__dirname, "../frontend/dist")));
-    app.get("*", (_, res) => {
-        res.sendFile(Path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-}
+// //make ready for deployment
+// if (ENV.NODE_ENV === "production") {
+//     app.use(express.static(Path.join(__dirname, "../frontend/dist")));
+//     app.get("*", (_, res) => {
+//         res.sendFile(Path.join(__dirname, "../frontend", "dist", "index.html"));
+//     });
+// }
 
-server.listen(PORT, () => {
-    console.log("Server is running on port: " + PORT)
-    connectDB()
-});
+// server.listen(PORT, () => {
+//     console.log("Server is running on port: " + PORT)
+//     connectDB()
+// });
